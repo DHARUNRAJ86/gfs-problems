c2896073319719ec9c66c2784fd92e19a0e5a2b3
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-03-06 22:17:41 +0530

    code

--- Daily Problems/practice.java
+++ Daily Problems/practice.java
@@ -11,3 +11,20 @@ class Solution {
         
     }
 }
+//Second Larges Element
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        int max1=Integer.MIN_VALUE;
+        int max2=Integer.MIN_VALUE;
+        for(int n:arr){
+            if(n>max1){
+                max2=max1;
+                max1=n;
+            }
+            else if(n>max2 && n!=max1){
+                max2=n;
+            }
+        }
+        return (max2==Integer.MIN_VALUE)?-1:max2;
+    }
+}
