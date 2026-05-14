6a6743bda13ecb7921f9abd4b63f19a6c0aababf
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-05-14 19:52:10 +0530

    add pattern1 implementation for number pattern generation

---
+++ Daily Problems/Patterns/patter1.java
@@ -0,0 +1,16 @@
+class pattern1{
+    public static void main(String[] args){
+        int n=5;
+        patterns(n);
+    }
+    public static void patterns(int n){
+        int num=1;
+         for(int i=0;i<n;i++) {
+        	 for(int j=0;j<=i;j++) {
+        		 System.out.print(num+" ");
+        		 num++;
+        	 }
+        	 System.out.println();
+         }
+    }
+}
